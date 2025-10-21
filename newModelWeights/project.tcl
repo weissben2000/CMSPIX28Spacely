e1b2024b0153046f7932efb982557a20415407c1
Anthony Badea <abadea@fasic-137645.fnal.gov>
2025-10-21 06:36:28 -0500

    Clean the MR to remove files that should not be in the central git.

--- newModelWeights/project.tcl
+++
@@ -1,8 +0,0 @@
-variable project_name
-set project_name "myproject"
-variable backend
-set backend "vivado"
-variable part
-set part "xcku115-flvb2104-2-i"
-variable clock_period
-set clock_period 5
